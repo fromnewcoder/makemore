dc68f74b0aee10354a325b4403d739e23e16bfd0
jq <jq@gmail.com>
2026-03-27 08:03:18 +0800

    rewrite stoi and itoi in a more Pythonic way with dictionary comprehension

--- makemore.py
+++ makemore.py
@@ -8,29 +8,22 @@ class MyDataSet:
             self._names = f.readlines()
         self._names = [ name.strip() for name in self._names]
         chars = sorted(list(set("".join(self._names))))
-        self.stoi = {}
-        self.itos = {}
-        for i, ch in enumerate(["."]+ chars):
-            self.stoi[ch] = i
-            self.itos[i] = ch
-        #print(self.stoi)   
-  
+        self.stoi = {ch: i for i, ch in enumerate(['.'] + chars)}
+        self.itos = {i: ch for ch, i in self.stoi.items()}
+    
         counts = torch.ones(27, 27)
 
         for name in self._names[:5]:
             print(name)
-            first = "."
-            for i,ch in enumerate(name + "."):
-                counts[self.stoi[first], self.stoi[ch]] += 1
-                print(f"{first} --> {ch}")
-                first = ch
+            nameWithStartEnd = "." + name + "."
+            for ch1, ch2 in zip(nameWithStartEnd, nameWithStartEnd[1:]):
+                counts[self.stoi[ch1], self.stoi[ch2]] += 1
+                print(f"{ch1} --> {ch2}")
 
-        row_sums = counts.sum(dim = 1, keepdim=True)
-        print(row_sums)
-        probs = counts/row_sums
-        print(probs)
+        self.probs = counts / counts.sum(dim = 1, keepdim=True)
+        #print(probs)
 
-
+    
 
 if __name__ == "__main__":
     md = MyDataSet()
